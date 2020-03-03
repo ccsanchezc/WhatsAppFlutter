ffd15aba141685ce2edd581b3190303cef58cd61
ccsanchezc <32348589+ccsanchezc@users.noreply.github.com>
2020-03-02 21:15:57 -0500

    Initial commit

---
+++ lib/pages/calls.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class CallPage extends StatefulWidget{
+  @override
+  CallState createState() {
+    // TODO: implement createState
+    return CallState();
+  }
+
+}
+
+class CallState extends State<CallPage>{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+
+    );
+  }
+
+}
